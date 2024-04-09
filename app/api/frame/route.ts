e2d5267b13d0478e5a1e3c1052a73e9892ad39fe
Adam Kraft <adamkraft1@gmail.com>
2024-04-09 14:35:53 -0500

    Try 15

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -6,6 +6,7 @@ import { addHyperFrame, getHyperFrame } from '../../hyperframes';
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   let accountAddress: string | undefined = '';
   let text: string | undefined = '';
+  let frame: string | undefined = '';
 
   const body: FrameRequest = await req.json();
   const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
@@ -21,6 +22,135 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   //  return new NextResponse('Message not valid', { status: 500 });
   //}
 
+  addHyperFrame('start', {
+    frame: getFrameHtmlResponse({
+      buttons: [
+        {
+          label: 'Road',
+        },
+        {
+          label: 'Woods',
+        },
+        {
+          label: 'Cave',
+        },
+        {
+          action: 'link',
+          label: 'TODO',
+          target: 'https://www.google.com',
+        },
+      ],
+      image: {
+        src: `${NEXT_PUBLIC_URL}/frame-1-forest.webp`,
+        aspectRatio: '1:1',
+      },
+      state: { frame: 'start' },
+      postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
+    }),
+    1: 'road',
+    2: 'woods-bear',
+    3: 'cave-1',
+  });
+  
+  addHyperFrame('road', {
+    frame: getFrameHtmlResponse({
+      buttons: [
+        {
+          label: 'Go Back',
+        },
+        {
+          label: 'Shack',
+        },
+        {
+          label: 'Forward',
+        },
+      ],
+      image: {
+        src: `${NEXT_PUBLIC_URL}/road.png`,
+        aspectRatio: '1:1',
+      },
+      postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
+    }),
+    1: 'start',
+    2: 'shack',
+    3: 'desert-road',
+  });
+
+  addHyperFrame('shack', {
+    frame: getFrameHtmlResponse({
+      buttons: [
+        {
+          label: 'Go Back',
+        },
+        {
+          label: 'Door',
+        },
+        {
+          label: 'Testing',
+        },
+      ],
+      image: {
+        src: `${NEXT_PUBLIC_URL}/shack.png`,
+        aspectRatio: '1:1',
+      },
+      input: {
+        text: 'What is the password?',
+      },
+      state: { frame: 'shack' },
+      postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
+    }),
+    1: 'road',
+    2: (text: string) => {
+      return text === 'All your Base are belong to you' ? 'key' : 'shack-bad-password';
+    },
+  });
+
+  addHyperFrame('shack-bad-password', {
+    frame: getFrameHtmlResponse({
+      buttons: [
+        {
+          label: 'Go Back',
+        },
+        {
+          label: 'Door',
+        },
+      ],
+      image: {
+        src: `${NEXT_PUBLIC_URL}/shack.png`,
+        aspectRatio: '1:1',
+      },
+      input: {
+        text: 'Try again. What is the password?',
+      },
+      state: { frame: 'shack-bad-password' },
+      postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
+    }),
+    1: 'road',
+    2: (text: string) => {
+      return text === 'All your Base are belong to you' ? 'key' : 'shack-bad-password';
+    },
+  });
+  
+  addHyperFrame('key', {
+    frame: getFrameHtmlResponse({
+      buttons: [
+        {
+          label: 'Go Back',
+        },
+        {
+          label: 'TODO',
+        },
+      ],
+      image: {
+        src: `${NEXT_PUBLIC_URL}/key.png`,
+        aspectRatio: '1:1',
+      },
+      state: { frame: 'key' },
+      postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
+    }),
+    1: 'shack',
+  });
+
   let state = { frame: 'start' };
 
   try {
@@ -30,17 +160,20 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     console.error(e);
   }
 
+  
 
-  /**
-   * Use this code to redirect to a different page
-   */
-  //if (message?.button === 3) {
-  //  return NextResponse.redirect(
-  //    'https://www.google.com/search?q=cute+dog+pictures&tbm=isch&source=lnms',
-  //    { status: 302 },
-  //  );
-  //}
+  if (!frame) {
+    return new NextResponse('Frame not found', { status: 404 });
+  }
+  
+  // There should always be a button number
+  if (!message?.button) {
+    return new NextResponse('Button not found', { status: 404 });
+  }
 
+  return new NextResponse(getHyperFrame(frame as string, text || '', message?.button));
+
+  /*
   return new NextResponse(
     getFrameHtmlResponse({
       buttons: [
@@ -63,6 +196,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
       postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
     }),
   );
+  */
 }
 
 export async function POST(req: NextRequest): Promise<Response> {
--- app/hyperframes.ts
+++ app/hyperframes.ts
@@ -32,3 +32,4 @@ export function getHyperFrame(frame: string, text: string, button: number) {
   
     return frames[nextFrameId].frame;
   }
+
