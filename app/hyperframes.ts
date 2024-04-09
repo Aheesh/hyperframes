e822218f9f749541be364efe4394469556e57f7b
Adam Kraft <adamkraft1@gmail.com>
2024-04-09 14:37:42 -0500

    Try 16

--- app/hyperframes.ts
+++ app/hyperframes.ts
@@ -33,3 +33,131 @@ export function getHyperFrame(frame: string, text: string, button: number) {
     return frames[nextFrameId].frame;
   }
 
+  addHyperFrame('start', {
+    frame: getFrameHtmlResponse({
+      buttons: [
+        {
+          label: 'Road',
+        },
+        {
+          label: 'Woods',
+        },
+        {
+          label: 'Cave',
+        },
+        {
+          action: 'link',
+          label: 'TODO',
+          target: 'https://www.google.com',
+        },
+      ],
+      image: {
+        src: `${NEXT_PUBLIC_URL}/frame-1-forest.webp`,
+        aspectRatio: '1:1',
+      },
+      state: { frame: 'start' },
+      postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
+    }),
+    1: 'road',
+    2: 'woods-bear',
+    3: 'cave-1',
+  });
+  
+  addHyperFrame('road', {
+    frame: getFrameHtmlResponse({
+      buttons: [
+        {
+          label: 'Go Back',
+        },
+        {
+          label: 'Shack',
+        },
+        {
+          label: 'Forward',
+        },
+      ],
+      image: {
+        src: `${NEXT_PUBLIC_URL}/road.png`,
+        aspectRatio: '1:1',
+      },
+      postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
+    }),
+    1: 'start',
+    2: 'shack',
+    3: 'desert-road',
+  });
+
+  addHyperFrame('shack', {
+    frame: getFrameHtmlResponse({
+      buttons: [
+        {
+          label: 'Go Back',
+        },
+        {
+          label: 'Door',
+        },
+        {
+          label: 'Testing',
+        },
+      ],
+      image: {
+        src: `${NEXT_PUBLIC_URL}/shack.png`,
+        aspectRatio: '1:1',
+      },
+      input: {
+        text: 'What is the password?',
+      },
+      state: { frame: 'shack' },
+      postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
+    }),
+    1: 'road',
+    2: (text: string) => {
+      return text === 'All your Base are belong to you' ? 'key' : 'shack-bad-password';
+    },
+  });
+
+  addHyperFrame('shack-bad-password', {
+    frame: getFrameHtmlResponse({
+      buttons: [
+        {
+          label: 'Go Back',
+        },
+        {
+          label: 'Door',
+        },
+      ],
+      image: {
+        src: `${NEXT_PUBLIC_URL}/shack.png`,
+        aspectRatio: '1:1',
+      },
+      input: {
+        text: 'Try again. What is the password?',
+      },
+      state: { frame: 'shack-bad-password' },
+      postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
+    }),
+    1: 'road',
+    2: (text: string) => {
+      return text === 'All your Base are belong to you' ? 'key' : 'shack-bad-password';
+    },
+  });
+  
+  addHyperFrame('key', {
+    frame: getFrameHtmlResponse({
+      buttons: [
+        {
+          label: 'Go Back',
+        },
+        {
+          label: 'TODO',
+        },
+      ],
+      image: {
+        src: `${NEXT_PUBLIC_URL}/key.png`,
+        aspectRatio: '1:1',
+      },
+      state: { frame: 'key' },
+      postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
+    }),
+    1: 'shack',
+  });
